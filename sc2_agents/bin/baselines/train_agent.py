7dfa1f41e31f83dbd65ac221635bf73a3061f62f
bbueno5000 <bbueno5000@gmail.com>
2018-08-15 00:54:40 -0600

    update

--- sc2_agents/bin/baselines/train_agent.py
+++ sc2_agents/bin/baselines/train_agent.py
@@ -25,24 +25,27 @@ from baselines import deepq
 from gym import make
 from gym_sc2 import envs
 
-def callback(lcl, _glb):
-    return lcl['t'] > 100 and sum(lcl['episode_rewards'][-101:-1]) / 100 >= 199
+def train_deepq_agent(env, network, **network_kwargs):
+    act = deepq.learn(
+        env,
+        network,
+        total_timesteps=1000,
+        **network_kwargs)
+    act.save('move_to_beacon_deepq_model_1.pkl')
+
+def train_ppo_agent(env, network, **network_kwargs):
+    act = deepq.learn(
+        env,
+        network,
+        total_timesteps=1000,
+        **network_kwargs)
+    act.save('move_to_beacon_deepq_model_1.pkl')
 
 def main(argv):
     env = make('MoveToBeacon-bbueno5000-v0')
-    model = deepq.models.mlp([64])
-    act = deepq.learn(
-        env,
-        q_func=model,
-        lr=1e-3,
-        max_timesteps=100000,
-        buffer_size=50000,
-        exploration_fraction=0.1,
-        exploration_final_eps=0.02,
-        print_freq=10,
-        callback=callback)
-    print('Saving model to move_to_beacon_deepq_model.pkl')
-    act.save('move_to_beacon_deepq_model.pkl')
+    network = deepq.models.mlp([64])
+    network_kwargs = {'num_actions': 64, 'scope': None}
+    train_deepq_agent(env, network, **network_kwargs)
 
 if __name__ == '__main__':
     app.run(main)
--- sc2_agents/bin/tensorforce/train_agent.py
+++ sc2_agents/bin/tensorforce/train_agent.py
@@ -1,4 +1,3 @@
-import gym
 # MIT License
 #
 # Copyright (c) 2018 Benjamin Bueno (bbueno5000)
@@ -21,37 +20,153 @@ import gym
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import gym_sc2
-import numpy as np
+"""
+OpenAI gym execution of TensorForce.
+"""
 
-_NO_OP = 0
-_PLAYER_NEUTRAL = 3
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from absl import app
+from absl import flags
+from json import load
+from logging import basicConfig as logging_basicConfig
+from logging import getLogger
+from logging import INFO
+from os import mkdir
+from os import path
+from gym_sc2 import envs
+from tensorforce import TensorForceError
+from tensorforce.agents import PPOAgent
+from tensorforce.execution import Runner
+from tensorforce.contrib.openai_gym import OpenAIGym
+from time import time
 
-class MoveToBeacon1d:
+FLAGS = flags.FLAGS
+flags.DEFINE_string('agent_config', None, "Agent configuration file")
+flags.DEFINE_bool('debug', False, "Show debug outputs")
+flags.DEFINE_bool('deterministic', False, "Choose actions deterministically")
+flags.DEFINE_integer('num_episodes', 10, "Number of episodes")
+flags.DEFINE_string('gym_id', None, "Id of the Gym environment")
+flags.DEFINE_string('job', None, "For distributed mode: The job type of this agent.")
+flags.DEFINE_string('load', None, "Load agent from this dir")
+flags.DEFINE_integer('max_episode_timesteps', None, "Maximum number of timesteps per episode")
+flags.DEFINE_string('monitor', None, "Save results to this directory")
+flags.DEFINE_bool('monitor_safe', False, "Do not overwrite previous results")
+flags.DEFINE_integer('monitor_video', 0, "Save video every x steps (0 = disabled)")
+flags.DEFINE_string('network', None, "Network specification file")
+flags.DEFINE_string('save', None, "Save agent to this dir")
+flags.DEFINE_integer('save_episodes', 100, "Save agent every x episodes")
+flags.DEFINE_float('sleep', None, "Slow down simulation by sleeping for x seconds (fractions allowed).")
+flags.DEFINE_integer('task', 0, "For distributed mode: The task index of this agent.")
+flags.DEFINE_bool('test', False, "Test agent without learning.")
+flags.DEFINE_integer('timesteps', None, "Number of timesteps")
+flags.DEFINE_bool('visualize', False, "Enable OpenAI Gym's visualization")
 
-    def __init__(self, visualize=False, step_mul=None) -> None:
-        self.env_name = "MoveToBeacon-bbueno5000-v0"
-        self.visualize = visualize
-        self.step_mul = step_mul
+def main(argv):
+    logging_basicConfig(level=INFO)
+    logger = getLogger(__file__)
+    logger.setLevel(INFO)
 
-    def get_action(self, env, obs):
-        neutral_y, neutral_x = (obs[0] == _PLAYER_NEUTRAL).nonzero()
-        if not neutral_y.any():
-            raise Exception('Beacon not found!')
-        target = [int(neutral_x.mean()), int(neutral_y.mean())]
-        return np.ravel_multi_index(target, obs.shape[1:])
+    environment = OpenAIGym(
+        gym_id='MoveToBeacon-bbueno5000-v0',
+        monitor=FLAGS.monitor,
+        monitor_safe=FLAGS.monitor_safe,
+        monitor_video=FLAGS.monitor_video,
+        visualize=FLAGS.visualize)
 
-    def run(self, num_episodes=1):
-        env = gym.make(self.env_name)
-        env.settings['visualize'] = self.visualize
-        env.settings['step_mul'] = self.step_mul
-        episode_rewards = np.zeros((num_episodes, ), dtype=np.int32)
-        for ix in range(num_episodes):
-            obs = env.reset()
-            done = False
-            while not done:
-                action = self.get_action(env, obs)
-                obs, _, done, _ = env.step(action)
-            episode_rewards[ix] = env.episode_reward
-        env.close()
-        return episode_rewards
+    # if FLAGS.agent_config is not None:
+    #     with open(FLAGS.agent_config, 'r') as fp:
+    #         agent_config = json.load(fp=fp)
+    # else:
+    #     raise TensorForceError(
+    #         "No agent configuration provided.")
+
+    # if FLAGS.network is not None:
+    #     with open(FLAGS.network, 'r') as fp:
+    #         network = json.load(fp=fp)
+    # else:
+    #     network = None
+    #     logger.info(
+    #         "No network configuration provided.")
+
+    network_spec = [
+        dict(type='flatten'),
+        dict(type='dense', size=32),
+        dict(type='dense', size=32)
+        ]
+
+    agent = PPOAgent(
+        states=environment.states,
+        actions=environment.actions,
+        network=network_spec
+        )
+
+    if FLAGS.load:
+        load_dir = path.dirname(FLAGS.load)
+        if not path.isdir(load_dir):
+            raise OSError(
+                "Could not load agent from {}: No such directory.".format(load_dir))
+        agent.restore_model(FLAGS.load)
+
+    if FLAGS.save:
+        save_dir = path.dirname(FLAGS.save)
+        if not path.isdir(save_dir):
+            try:
+                mkdir(save_dir, 0o755)
+            except OSError:
+                raise OSError(
+                    "Cannot save agent to dir {} ()".format(save_dir))
+
+    if FLAGS.debug:
+        logger.info("-" * 16)
+        logger.info("Configuration:")
+        logger.info(agent)
+
+    runner = Runner(
+        agent=agent,
+        environment=environment,
+        repeat_actions=1)
+
+    if FLAGS.debug:
+        report_episodes = 1
+    else:
+        report_episodes = 100
+
+    logger.info(
+        "Starting {agent} for Environment {env}".format(
+            agent=agent, env=environment))
+
+    def episode_finished(r, id_):
+        if r.episode % report_episodes == 0:
+            steps_per_second = r.timestep / (time() - r.start_time)
+            logger.info("Finished episode {:d} after {:d} timesteps. Steps Per Second {:0.2f}".format(
+                r.agent.episode, r.episode_timestep, steps_per_second))
+            logger.info("Episode reward: {}".format(r.episode_rewards[-1]))
+            logger.info("Average of last 500 rewards: {:0.2f}".format(
+                sum(r.episode_rewards[-500:]) / min(500, len(r.episode_rewards))))
+            logger.info("Average of last 100 rewards: {:0.2f}".format(
+                sum(r.episode_rewards[-100:]) / min(100, len(r.episode_rewards))))
+        if FLAGS.save and FLAGS.save_episodes is not None and not r.episode % FLAGS.save_episodes:
+            logger.info("Saving agent to {}".format(FLAGS.save))
+            r.agent.save_model(FLAGS.save)
+        return True
+
+    runner.run(
+        num_timesteps=FLAGS.timesteps,
+        num_episodes=FLAGS.num_episodes,
+        max_episode_timesteps=FLAGS.max_episode_timesteps,
+        deterministic=FLAGS.deterministic,
+        episode_finished=episode_finished,
+        testing=FLAGS.test,
+        sleep=FLAGS.sleep)
+
+    runner.close()
+
+    logger.info("Learning completed.")
+    logger.info("Total episodes: {ep}".format(ep=runner.agent.episode))
+
+if __name__ == '__main__':
+    app.run(main)
+
+# python openai_gym/move_to_beacon.py --gym_id SC2MoveToBeacon-bbueno5000-v0 --agent_config openai_gym/configs/vpg.json --network openai_gym/configs/mlp2_network.json
